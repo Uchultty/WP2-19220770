67ca236e0769f8d971b026d2cf61e7cac0ce77a4
Uchultty <dlintang456@gmail.com>
2023-10-06 17:42:37 +0700

    Menambahkan File Ke Git

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Damar
+        saya tinggal di daerah kebalen
+        olahraga yang saya sukai bermain bola";
+    }
+}
---
+++ application/views/view-latihan.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+        <tittle>Latihan 1</tittle>
+</head>
+
+<body>
+    Halo kawan.. yuk kita belajar web program<br>
+    Nilai 1 =<?= $nilai1; ?>
+    Nilai 2 =<?= $nilai2; ?>
+    Ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+    $nilai1 ." + " . $nilai2 . " = " . $hasil; ?>
+
+</body>
+
+</html>
