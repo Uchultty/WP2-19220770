9a09be2ddc1c633011fecccf756efc873c257636
Uchultty <dlintang456@gmail.com>
2023-10-20 15:29:36 +0700

    menambahkan file baru

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -2,24 +2,25 @@
    <h1>
       <?php echo $judul ?>
    </h1>
+   <center><img src="<?php echo base_url() ?>assets/img/totogacor.jpeg" width="300px" height="400px"></center>
    <h4>Nama</h4>
    <ul type="disc">
-      <li>Nama Depan : Imam</li>
-      <li>Nama Belakang : Nawawi</li>
+      <li>Nama Depan : Damar</li>
+      <li>Nama Belakang : Ganteng</li>
    </ul>
    <br>
    <h4>Alamat</h4>
    <ul type="none">
-      <li> Jalan Ciledug Raya No. 168 Pesanggrahan</li>
+      <li> Jalan Flamboyan Raya 3</li>
    </ul>
 
    <h4>Tempat Lahir</h4>
    <ul type="none">
-      <li>Cirebon</li>
+      <li>Planet Bekasi</li>
    </ul>
    <h4>Olah Raga Favorit</h4>
    <ul type="square">
-      <li>Bulutangkis</li>
-      <li>Catur</li>
+      <li>Futsal</li>
+      <li>Game</li>
    </ul>
 </section>
